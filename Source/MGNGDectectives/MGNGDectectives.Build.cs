1b5f1a52c3a4cbe517cf23c3d9cc060db055774b
Ketosvaldo <ketchulo.conk2013@gmail.com>
2023-05-04 14:33:01 -0600

    First Commit

---
+++ Source/MGNGDectectives/MGNGDectectives.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MGNGDectectives : ModuleRules
+{
+	public MGNGDectectives(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}
